6456999284f029789d953fb9a3f3e96e92c8656a
Cainã Bonates <131978490+Cainabo@users.noreply.github.com>
2026-02-20 13:37:13 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -198,8 +198,8 @@ def comparar_com_baseline(url):
 
 import time
 
-# INTERVALO = 60 * 60 * 12  # produção (12h)
-INTERVALO = 60 * 2 #2 em 2 minutos
+INTERVALO = 60 * 60 * 12  # produção (12h)
+#INTERVALO = 60 * 2 #2 em 2 minutos
 
 def executar_monitoramento():
     if not os.path.exists(BASELINE_DIR):
